6f9949575fad3b527f5730cb447b5bac318c9cba
phreelyfe <phreelyfe@gmail.com>
2015-02-07 05:51:57 -0400

    Added Browser Compatability check & fixed page heights

--- public/js/main.js
+++ public/js/main.js
@@ -2,18 +2,20 @@ $(document).ready(function() {
     // GLOBAL FUNCTIONS
 
     // DETECT USERS BROWSER
+    var userAgent;
     if (navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1) {
-        var userAgent = "safari";
+        userAgent = "safari";
     } else if (navigator.userAgent.indexOf('Firefox') != -1 && navigator.userAgent.indexOf('Safari') == -1) {
-        var userAgent = "firefox";
+        userAgent = "firefox";
     } else if (navigator.userAgent.indexOf('Chrome') != -1 && navigator.userAgent.indexOf('Firefox') == -1) {
-        var userAgent = "chrome";
+        userAgent = "chrome";
     } else {
-        var userAgent = "unknown";
+        userAgent = "unknown";
     }
     // console.log("Your Browser Is:", userAgent);
-    var browser = document.body.attributes.browser;
-    browser.value = userAgent;
+    var page = document.getElementsByClassName('content page')[0];
+    page.setAttribute('browser', userAgent);
+    var browser = page.attributes.browser;
 
     // NAVBAR FIXED AFTER SCROLL
     function fixNavbarToTop() {
@@ -23,20 +25,19 @@ $(document).ready(function() {
             var navbar = $(document.getElementById('navbar'));
 
             if (scrollPos >= navbar[0].offsetTop) {
-                // console.log("Greater Than");
-                $(navbar).addClass('navbar-fixed-top')
-            } else if (scrollPos <= 400) {
-                // console.log("Less Than");
+                // console.log("Greater Than Nav Offset", navbar[0].offsetTop);
+                $(navbar).addClass('navbar-fixed-top');
+            } else if (scrollPos < navbar[0].offsetTop) {
+                // console.log("Less Than Nav Offset", navbar[0].offsetTop);
                 $(navbar).offsetTop = 400;
-                $(navbar).removeClass('navbar-fixed-top')
-                    // console.log($(navbar));
+                $(navbar).removeClass('navbar-fixed-top');
             } else {
-                console.log("nothin to be done")
+                // console.log("nothin to be done", navbar[0].offsetTop)
                 console.log(navbar[0].offsetTop - navbar[0].offsetHeight);
             }
 
-        }, 100)
-    };
+        }, 100);
+    }
 
     // RUN NAVBAR FIX
     if (document.getElementById('home-page'))
@@ -50,23 +51,27 @@ $(document).ready(function() {
         $("#teaser-window").height(windowHeight);
     }
 
-    if (window.scrollY > 500 /*navbar[0].offsetTop*/ ) {
-        console.log("Window Height", windowHeight);
-        console.log(navbar);
-    }
-
     $(window).scroll(function() {
         var x = $(this).scrollTop();
         // console.log("Chaning to this pos:",parseInt(x / 5) + 'px');
         $('#teaser-window').css('background-position', 'center -' + parseInt(x / 2) + 'px');
     });
 
+    // LOGIN
+    var loginBtn = $('#signin');
+    var createAccount = $('.not-a-member');
+    loginBtn.on('click', function() {
+    	alert('Login Comming Soon!')
+    });
+    createAccount.on('click', function(e){
+    	alert("Creat Account Comming Soon!")
+    })
+
     // PROFILE PAGE MODAL
     $('.modal-btn').on('click', function(e) {
         // console.log("Clicked", e);
         e.preventDefault();
 
-        var modal = $('.modal-dialog');
         var modalTitle = $('.modal-title');
         var modalBody = $('.modal-body');
         var modalFooter = $('.modal-footer');
@@ -75,14 +80,14 @@ $(document).ready(function() {
         var imageTitle = e.target.attributes.alt.value;
 
         $(img).addClass('img-responsive');
-        img.onload = function(e) { /*console.log("I'm Loaded");*/ }
+        img.onload = function() { /*console.log("I'm Loaded");*/ };
         img.src = e.target.src;
 
         modalTitle.html(imageTitle);
         modalBody.html(img);
         modalFooter.html("<button type='button' class='btn btn-default' data-dismiss='modal'>Close</button>");
 
-        console.log(modalBody)
+        // console.log(modalBody)
     });
 
     // SUPPORT PAGE CHATBOX
@@ -94,25 +99,25 @@ $(document).ready(function() {
         var usersOnline = document.getElementById('users');
 
         if (browser.value == "safari") {
-            chatWindow.style.height = "573px"
-            usersOnline.style.height = "573px"
+            chatWindow.style.height = "573px";
+            usersOnline.style.height = "573px";
             chatWindow.style.maxHeight = "573px";
-            console.log("Safari Detected! Changing window height to:" + chatWindow.style.height)
+            // console.log("Safari Detected! Changing window height to:" + chatWindow.style.height)
         } else if (browser.value == "chrome") {
-            chatWindow.style.height = "540px"
-            usersOnline.style.height = "540px"
+            chatWindow.style.height = "540px";
+            usersOnline.style.height = "540px";
             chatWindow.style.maxHeight = "540px";
-            console.log("Chrome Detected! Changing window height to:" + chatWindow.style.height)
+            // console.log("Chrome Detected! Changing window height to:" + chatWindow.style.height)
         } else if (browser.value == "firefox") {
-            chatWindow.style.height = "530px"
-            usersOnline.style.height = "530px"
+            chatWindow.style.height = "530px";
+            usersOnline.style.height = "530px";
             chatWindow.style.maxHeight = "530px";
-            console.log("Firefox Detected! Changing window height to:" + chatWindow.style.height)
+            // console.log("Firefox Detected! Changing window height to:" + chatWindow.style.height)
         } else {
-            chatWindow.style.height = "570px"
-            usersOnline.style.height = "570px"
+            chatWindow.style.height = "570px";
+            usersOnline.style.height = "570px";
             chatWindow.style.maxHeight = "570px";
-            console.log("No Browser Detected! Changing window height to:" + chatWindow.style.height)
+            // console.log("No Browser Detected! Changing window height to:" + chatWindow.style.height)
         }
 
         message.keyup(function(e) {
@@ -126,7 +131,7 @@ $(document).ready(function() {
         sendBtn.on('click', function() {
             var message = $('#message').val();
 
-            if (message == null || message == undefined || message == "") {
+            if (message === null || message === undefined || message === "") {
                 console.warn("Please Enter a Message");
             } else {
 	            $('#chat').append("Guest: " + message + "\n" + "<br />");
