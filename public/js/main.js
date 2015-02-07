e17c7b07ec7920af9bcb2f89d0eed8a35ec7c11a
phreelyfe <phreelyfe@gmail.com>
2015-02-06 21:24:22 -0400

    Portfolio page and Support page mocked. Home Page Parallax Fix and Grid Layout near complete

--- public/js/main.js
+++ public/js/main.js
@@ -1,23 +1,49 @@
-function showScroll() {
-    setInterval(function() {
-        console.log(window.scrollY);
-        var cHeight = $('#currentHeight');
-        cHeight.innerHTML = window.scrollY;
-    }, 1000)
-};
+$(document).ready(function() {
+    // GLOBAL FUNCTIONS
 
-$(document).ready(function(e) {
-    showScroll();
-    
-    var origheight = $("#trans1").height();
-    var height = $(window).height();
-    
-    if (height > origheight) {
-        $("#trans1").height(height);
+    // NAVBAR FIXED AFTER SCROLL
+    function fixNavbarToTop() {
+        setInterval(function() {
+            var scrollPos = window.scrollY;
+            // console.log(scrollPos);
+            var navbar = $(document.getElementById('navbar'));
+
+            if (scrollPos >= navbar[0].offsetTop) {
+                // console.log("Greater Than");
+                $(navbar).addClass('navbar-fixed-top')
+            } else if (scrollPos <= 400) {
+                // console.log("Less Than");
+                $(navbar).offsetTop = 400;
+                $(navbar).removeClass('navbar-fixed-top')
+                    // console.log($(navbar));
+            } else {
+                console.log("nothin to be done")
+                console.log(navbar[0].offsetTop - navbar[0].offsetHeight);
+            }
+
+        }, 100)
+    };
+
+    // RUN NAVBAR FIX
+	if (document.getElementById('home-page'))
+		fixNavbarToTop();
+
+    // PARALLAX EFFECT ON TEASER WINDOW
+    var teaserWindow = $("#teaser-window").height();
+    var windowHeight = $(window).height();
+
+    if (windowHeight > teaserWindow) {
+        $("#teaser-window").height(windowHeight);
+    }
+
+    if (window.scrollY > 500 /*navbar[0].offsetTop*/ ) {
+        console.log("Window Height", windowHeight);
+        console.log(navbar);
     }
 
     $(window).scroll(function() {
         var x = $(this).scrollTop();
-        $('#trans1').css('background-position', 'center -' + parseInt(x / 5) + 'px');
+        // console.log("Chaning to this pos:",parseInt(x / 5) + 'px');
+        $('#teaser-window').css('background-position', 'center -' + parseInt(x / 2) + 'px');
     });
 });
