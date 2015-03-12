459ae4070b3d5a0630885e1f4f2d913573aea7de
phreelyfe <phreelyfe@gmail.com>
2015-03-12 10:50:03 -0300

    Working heroku. changed Socket port from 3K to heroku domain

--- public/services/socket.io.service.js
+++ public/services/socket.io.service.js
@@ -1,6 +1,6 @@
 angular.module('tnApp.services', []).service("Socket", ['$state', '$rootScope', '$window', '$q', function($state, $rootScope, $window, $q) {
     
-    return io.connect('http://localhost:3000');
+    return io.connect('https://tech-ninja.herokuapp.com');
     // return io.connect('http://portfolio-site-techninja-1.c9.io/')
 
 
